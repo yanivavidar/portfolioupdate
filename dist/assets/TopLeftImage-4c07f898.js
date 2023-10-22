1da169459e18673524abdf33a5489343a1f6da30
yanivavidar <139516615+yanivavidar@users.noreply.github.com>
2023-10-22 18:58:36 +0300

    Add files via upload

---
+++ dist/assets/TopLeftImage-4c07f898.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-b382a451.js";const o=()=>t.jsx("div",{className:"fixed left-0 top-0 mix-blend-color-dodge z-10 w-[200px] xl:w-[400px] opacity-50",children:t.jsx("img",{src:"/top-left-img.png",width:400,height:400,alt:"top-left-image"})});export{o as default};
